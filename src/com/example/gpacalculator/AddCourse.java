36b9f19950a4d0773d54035630072079454a5368
madhavc <chhura.madhav@gmail.com>
2014-06-04 14:29:18 -0700

    Started a GPA Calculating App
    This app is to help me learn more about the android.

---
+++ src/com/example/gpacalculator/AddCourse.java
@@ -0,0 +1,16 @@
+package com.example.gpacalculator;
+
+import android.app.Activity;
+import android.view.View;
+
+public class AddCourse extends Activity implements View.OnClickListener{
+
+	@Override
+	public void onClick(View v) {
+
+		
+	}
+
+	
+
+}
